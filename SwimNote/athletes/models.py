f4b4c93de3432493e938b152010c4903d1e2cce1
DiDinar5 <dinar-wp9@mail.ru>
2022-06-18 12:12:05 +0300

    athletes model

---
+++ SwimNote/athletes/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class SexField(models.TextChoices):
+    MALE = 'М'
+    FEMALE = 'Ж'
+
+class StyleField(models.TextChoices):
+    FREESTYLE = 'кроль'
+    BUTTERFLY = 'дельфин'
+    BACKSTROKE = 'на спине'
+    BREASTSTROKE = 'брасс'
+    MEDLEY = 'комплекс'
+
+class Athlete(models.Model):
+    name = models.CharField(max_length=50)
+    extra = models.CharField(max_length=500, null=True)
+    birthday = models.DateField()
+    sex = models.CharField(max_length=1,
+                           choices = SexField.choices,
+                           default = SexField.MALE)
+    style = models.CharField(max_length=16,
+                             choices = StyleField.choices,
+                             default = StyleField.FREESTYLE)
+
