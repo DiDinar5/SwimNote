0fe41a56e1a963b2ffb6f1e5a3bd17cff30a5632
DiDinar5 <dinar-wp9@mail.ru>
2022-07-03 16:10:22 +0300

    athletes/index.html

--- SwimNote/athletes/urls.py
+++ SwimNote/athletes/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . import views
+from athletes import views
 
 urlpatterns = [
     path('', views.index, name='index'),
--- SwimNote/athletes/views.py
+++ SwimNote/athletes/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
-
-
+from django.template import loader
+from athletes.models import Athlete,PersonalBest
 def index(request):
-    return HttpResponse("Физкульт привет,атлетам.")
+    all = Athlete.objects.order_by('birthday')[:]
+    template = loader.get_template('index.html')
+    context = {
+        'athletes': all,
+    }
+    #str = ', '.join([a.name for a in all])
+    return HttpResponse(template.render(context, request))
+
+
+
 
